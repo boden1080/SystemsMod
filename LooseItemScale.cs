3ac78324c530be1c1a55fa09553f0bdeba9ece32
boden1080 <45050369+boden1080@users.noreply.github.com>
2019-04-01 11:36:55 +0100

    Update LooseItemScale.cs

--- LooseItemScale.cs
+++ LooseItemScale.cs
@@ -18,20 +18,22 @@ namespace Self_Checkout_Simulator
 
         public bool IsEnabled()
         {
-            // TODO
-            return false;
+            if (enabled == true)
+                return true;
+            else
+                return false;
         }
 
         public void LinkToSelfCheckout(SelfCheckout sc)
         {
-            selfCheckout = sc;
+            this.selfCheckout = sc;
         }
 
         // NOTE: In reality the weight wouldn't be passed in here, the
         //       scale would detect the change and notify the self checkout
         public void WeightChangeDetected(int weight)
         {
-            selfCheckout.LooseItemAreaWeightChanged(weight);
+            
         }
     }
 }
